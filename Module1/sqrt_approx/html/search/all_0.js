1112dc2556be4e92f10a9d842affe44c9aaab051
dastcvi <david.stclair@colorado.edu>
2018-02-15 18:22:58 -0700

    initial commit with finished module 1 code

---
+++ Module1/sqrt_approx/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main',['main',['../main_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main.c']]],
+  ['main_2ec',['main.c',['../main_8c.html',1,'']]],
+  ['my_5fsqrt',['my_sqrt',['../main_8c.html#ab530b5c5abd6ec3f886f2b48e3905ee1',1,'main.c']]]
+];
---
+++ Module1/sqrt_approx/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main',['main',['../main_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main.c']]],
+  ['my_5fsqrt',['my_sqrt',['../main_8c.html#ab530b5c5abd6ec3f886f2b48e3905ee1',1,'main.c']]]
+];
